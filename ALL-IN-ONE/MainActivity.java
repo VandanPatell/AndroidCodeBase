050b04a13b1c6da12f0ffdee20b704584f7da947
Vandan Patel <59683751+VandanPatell@users.noreply.github.com>
2022-04-25 01:10:28 +0530

    Update MainActivity.java

--- ALL-IN-ONE/MainActivity.java
+++ ALL-IN-ONE/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.vandan_login_db;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     EditText et_username, et_password;
-    Button btn_login;
+    Button btn_login,btn_database;
 
+    Spinner spinner;
+    Button calender_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
         btn_login = findViewById(R.id.btn_login);
+        btn_database = findViewById(R.id.btn_Database);
+        calender_btn=findViewById(R.id.calender);
+
+
+        calender_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this);
+                    datePickerDialog.setOnDateSetListener(MainActivity.this);
+                    datePickerDialog.show();
+                }
+            }
+        });
+
+
+        spinner=findViewById(R.id.sp_choices);
+        ArrayAdapter adp=new ArrayAdapter(this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.user_choices));
+        spinner.setAdapter(adp);
+//        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//        });
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +89,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_database.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,databaseactivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        Log.d("date", String.valueOf(month));
+    }
 }
